09f0191b2720837dd6e691b1162fe30eb0cea798
ivan-risueno <ivan.risueno@estudiantat.upc.edu>
2023-09-22 12:36:25 +0200

    Definición de cuerpo finito y operaciones básicas funcionando

--- Lab2/G_F.py
+++ Lab2/G_F.py
@@ -12,22 +12,67 @@ class G_F:
         #tenga el valor i tal que a=g**i. (g generador del cuerpo finito
         #representado por el menor entero entre 0 y 255.)
 
-        self.Polinomio_Irreducible = Polinomio_Irreducible
-        self.Tabla_EXP = generarTablaEXP()
-        self.Tabla_LOG = generarTablaLOG()
-    
-    def generarTablaEXP():
-        return
+        generador = self.encontrarGenerador()
 
-    def generarTablaLOG():
-        return
+        self.Polinomio_Irreducible = Polinomio_Irreducible
+        self.Tabla_EXP, self.Tabla_LOG = self.generarTablas(generador)
+
+    def encontrarGenerador(self):
+        for generador in range(2, 256):
+            es_generador = True
+            elemento = 1
+            for _ in range(8):
+                elemento = (elemento * generador) % 256
+                if elemento == 1:
+                    es_generador = False
+                    break
+            if es_generador:
+                return generador
+
+        return None
+
+    def productoLento(self, a, b):
+        c = 0
+        for _ in range(8):
+            if b & 1:
+                c ^= a
+
+            bit_mas_alto = a & 128
+            a <<= 1
+            if bit_mas_alto:
+                a ^= self.Polinomio_Irreducible
+
+            b >>= 1
+        return c
+
+    def generarTablas(self, generador):
+        EXP = {}
+        LOG = {}
+        while generador < 255:
+            elemento = 1
+            bucle = False
+            for i in range(255):
+                EXP[i] = elemento
+                LOG[elemento] = i
+                elemento = self.productoLento(elemento, generador)
+                if elemento == 1 and i < 254:
+                    bucle = True
+                    break
+            if not bucle:
+                break
+            generador += 1
+        return EXP, LOG
 
     def xTimes(self, n):
 
         #Entrada: un elemento del cuerpo representado por un entero entre 0 y 255
         #Salida: un elemento del cuerpo representado por un entero entre 0 y 255
         #que es el producto en el cuerpo de ’n’ y 0x02 (el polinomio X).
-        return
+
+        if n < 128:
+            return n << 1
+
+        return (n << 1) ^ 256
 
     def producto(self, a, b):
 
@@ -37,7 +82,8 @@ class G_F:
         #Atenci ́on: Se valorar ́a la eficiencia. No es lo mismo calcularlo
         #usando la definici ́on en t ́erminos de polinomios o calcular
         #usando las tablas Tabla_EXP y Tabla_LOG.
-        return
+
+        return self.Tabla_EXP[(self.Tabla_LOG[a] + self.Tabla_LOG[b]) % 255]
 
     def inverso(self, n):
 
@@ -46,4 +92,14 @@ class G_F:
         #el inverso multiplicativo de n representado por un entero entre
         #1 y 255 si n <> 0.
         #Atenci ón: Se valorar ́a la eficiencia.
-        return
+
+        if n == 0:
+            return 0
+
+        return self.Tabla_EXP[255 - self.Tabla_LOG[n]]
+
+a = G_F()
+
+print(a.productoLento(12,240))
+print(a.producto(12, 240))
+print(a.inverso(11))
